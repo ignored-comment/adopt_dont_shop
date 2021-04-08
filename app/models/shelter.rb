f9f88ea306e29bcae0373350d6d4f722f2688d77
ignored-comment <info.konham@gmail.com>
2021-04-07 21:31:40 -0400

    Create model test for shelter method alphabetically...by_name

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -16,6 +16,10 @@ class Shelter < ApplicationRecord
       .order("pets_count DESC")
   end
 
+  def self.alphabetically_descending_by_name
+    select("shelters.*").order("name DESC")
+  end
+
   def pet_count
     pets.count
   end
